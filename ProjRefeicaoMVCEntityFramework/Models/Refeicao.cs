5d2fd1bce1a5e2a7ab0e1112550326321644f22a
MarceloTonhetti <marcelotjunior2507@gmail.com>
2021-03-23 23:56:36 -0300

    Creating Reficao and Pizza models

---
+++ ProjRefeicaoMVCEntityFramework/Models/Refeicao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjRefeicaoMVCEntityFramework.Models
+{
+	public abstract class Refeicao
+	{
+		public int Id { get; set; }
+		public string Description { get; set; }
+		public decimal Value { get; set; }
+	}
+}
